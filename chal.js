fec502286950c70a57bacfbd7c1b226453541a8a
Aris Jirat <arisjiratkurniawan@gmail.com>
2020-02-12 11:00:49 +0700

    works with headless

---
+++ chal.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+const log = console.log;
+ 
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+ 
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold(chalk.white(' Coba ')));
